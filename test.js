20f58e3cc556fe68a6df2ec1cdd90a2d82f56d23
dtumkursreenivas <dtumkursreenivas@apple.com>
2017-02-17 14:29:00 -0800

    test

--- test.js
+++ test.js
@@ -1,7 +1,39 @@
 function test(x){
 	return x * x ;
 }
-
 var date = new Date(2009,11,2);
+var te = new Date(100,12,3);
 
-console.log(date)
+
+
+console.log(te)
+
+function testify(y){
+	return y*y;
+}
+console.log(testify(10));
+
+
+function testcase(name,tests){
+	
+	for(var i in tests){
+		console.log(i);
+
+	}
+
+	for(var i=0;i<10;i++){
+		console.log(i);
+	}
+
+
+
+}
+
+
+testcase("name",["darshan","murthy"]);
+
+
+
+
+
+
