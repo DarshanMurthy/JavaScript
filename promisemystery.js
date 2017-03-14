6b7395857e969b23e3bb250e99c89997193f90f9
dtumkursreenivas <dtumkursreenivas@apple.com>
2017-03-14 11:38:28 -0700

    added the function

--- promisemystery.js
+++ promisemystery.js
@@ -1,5 +1,12 @@
-var name1 = function node(name1, name2){
-	this.name1 = name1;
+testNode = function(name1,name2){
+	this.name1= name1;
 	this.name2 = name2;
 }
+test = new testNode("name1","name2");
+console.log(test)
 
+
+test = function node(name1){
+	this.name1=name1;
+}
+test("name");
