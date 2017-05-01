1c310620e43945b577a72c2ae85224565deb2b2a
Darshu <darshants.darshan1@gmail.com>
2017-04-30 21:01:31 -0700

    removed the WORK

---
+++ practice.js
@@ -0,0 +1,4 @@
+var p1 = Promise.resolve(3)
+var p2 = Promise.resolve(4*6)
+var p3 = Promise.resolve(8*8)
+console.log(p3)
--- promiseTest.js
+++ promiseTest.js
@@ -1,4 +1,15 @@
 temp=Object.assign({},{"name":"darshan"})
 temp2=Object.assign({},{"temp":"darshan"})
 var p1 = Promise.resolve(temp2)
-console.log(p1)
+temp=Promise.resolve(9*9+1)
+visit = Promise.resolve(9*4+9)
+visitTest = Promise.resolve(8*8)
+console.log(visitTest)
+
+var temp23 = function(val){
+  val =val+10;
+  value = return Promise.resolve(100*9)
+
+  return value
+}
+console.log(temp23(19))
