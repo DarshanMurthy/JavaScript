5cdcca320b26773377626f1394a4da912db55e7b
Darshu <darshants.darshan1@gmail.com>
2017-05-02 11:14:03 -0700

    added the commit

--- promiseTestCall.js
+++ promiseTestCall.js
@@ -4,10 +4,20 @@ var p3 = Promise.resolve(8*8)
 var p4 = Promise.resolve(0/0)
 var p5 = Promise.resolve(8*8)
 
+var p6  = new Promise((resolve,reject)=>{
+    setTimeout(resolve,100,'foo')
+})
+console.log(p6)
+
+
 Promise.all([p1,p2,p3,p4,p5]).then(values =>{
     console.log(values[0])
 }
 );
+
+Promise.all([p1,p2]).then(values=>{
+    console.log(values)
+} )
 
 
 
